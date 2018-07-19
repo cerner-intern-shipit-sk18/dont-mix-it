da36650c07a1a081d2c3a5cdcec3ffe3534653fe
Jacob Pickle <jmp6xb@mst.edu>
2018-07-19 17:32:23 -0500

    Revert "Revert "gonna retarget maybe""
    This reverts commit 6d851f1ecc627c7d56d11b297d179660c0ac075e.

---
+++ dontmixit/feature/src/main/java/com/example/dontmixit/feature/Drug.java
@@ -0,0 +1,13 @@
+package com.example.dontmixit.feature;
+
+public class Drug
+{
+    String m_strName;
+    String m_strDetails;
+
+    public Drug(String name, String details)
+    {
+        m_strDetails = details;
+        m_strName = name;
+    }
+}
--- dontmixit/feature/src/main/java/com/example/dontmixit/feature/InteractionsActivity.java
+++ dontmixit/feature/src/main/java/com/example/dontmixit/feature/InteractionsActivity.java
@@ -3,9 +3,12 @@ package com.example.dontmixit.feature;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class InteractionsActivity extends AppCompatActivity {
+
+    boolean InteractionOkay = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +17,19 @@ public class InteractionsActivity extends AppCompatActivity {
 
         TextView txtView = (TextView)findViewById(R.id.textView);
         txtView.setText(getIntent().getStringExtra("InteractionDetails"));
+
+        InteractionOkay = getIntent().getBooleanExtra("InteractionOkay", false);
+
+        ImageView imgView = (ImageView)findViewById(R.id.imageView);
+        if(InteractionOkay)
+        {
+            imgView.setImageResource(R.drawable.good);
+        }
+        else
+        {
+            imgView.setImageResource(R.drawable.bad);
+        }
+
     }
 
     public void onClickHome(android.view.View view)
